a10bb8e59ab4efcef7f31a877b9cbd5bab3d9cb7
in0ho1no <yin0ho1no@gmail.com>
2026-04-18 22:01:39 +0900

    ・AI Agentから利用しやすいようにdev向けパッケージ追加 ・伴って既存の体裁修正

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+"""Example module."""
+
+
 def function_example(arg1: str, arg2: int) -> None:
-    """Example function that takes a string and an integer as arguments"""
+    """Example function that takes a string and an integer as arguments."""
     print(f'arg1: {arg1}, arg2: {arg2}')
 
 
 def main() -> None:
-    """Main function"""
+    """Main function."""
     print('Hello, World!')
     function_example('hello world', 42)
 
